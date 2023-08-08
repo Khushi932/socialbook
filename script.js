2198b69f6ea471f5b17713bc9a335ffd868ae9f8
Khushi Patel <khuship0903@gmail.com>
2023-08-08 14:00:07 +0530

    only dark mode left

---
+++ script.js
@@ -0,0 +1,9 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn")
+
+function settingdMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
+}
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle(dark-btn-on);
+}
